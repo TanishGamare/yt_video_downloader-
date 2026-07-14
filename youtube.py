ac609357d888e8d8b4134af8866474e5f8095285
TanishGamare <tanishgamare20220@gmail.com>
2026-07-14 10:43:53 +0530

    replaced pytube with yt-dlp for quality of life change

--- youtube.py
+++ youtube.py
@@ -1,15 +1,21 @@
-from pytube import YouTube 
+import yt_dlp  
 import tkinter as tk 
 from tkinter import filedialog 
 
 def download_video(url, save_path):
+
+    ydl_opts = {
+        "format": "bestvideo+bestaudio/best",
+        "outtmpl": f"{save_path}/%(title)s.%(ext)s",
+    }
+
     try:
-        yt = YouTube(url)
-        streams = yt.streams.filter(progressive=True, file_extension="mp4")
-        highest_res_stream = streams.get_highest_resolution()
-        highest_res_stream.download(output_path=save_path)
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+
         print("Video Downloaded Successfully!")
-    except Exception as e: 
+
+    except Exception as e:
         print(e)
 
 def open_file_dialogue():
